66070dd45946bfc125825d7ffdd6403385263259
Alessio Palmero Aprosio <alessio@apnetwork.it>
2021-11-30 14:37:57 +0100

    Added confusion matrix

--- 06-micro_macro.py
+++ 06-micro_macro.py
@@ -1,13 +1,18 @@
 import logging
 import argparse
+import numpy as np
+import operator
+import collections
 
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import confusion_matrix
 
 parser = argparse.ArgumentParser(description='Get micro/macro.')
 parser.add_argument("pred_file", metavar="pred-file", help="Predictions file")
 parser.add_argument("gold_file", metavar="gold-file", help="Gold file")
 parser.add_argument("--prob-threshold", help="Probability threshold (default: 0.5)", type=float, default=0.5)
+parser.add_argument("--show-cm", help="Show confusion matrix", action='store_true')
 
 args = parser.parse_args()
 
@@ -61,3 +66,61 @@ micro_f1_score = f1_score(m.transform(y_true), m.transform(y_pred), average='mic
 
 print("Macro:", macro_f1_score)
 print("Micro:", micro_f1_score)
+
+if args.show_cm:
+    print()
+    y_true = [x[0] for x in y_true]
+    y_pred = [x[0] for x in y_pred]
+
+    distribution = {i:y_true.count(i) for i in set(y_true)}
+    sorted_x = sorted(distribution.items(), key=operator.itemgetter(1), reverse=True)
+    distribution = collections.OrderedDict(sorted_x)
+
+    labels = set()
+    for l in y_true:
+        labels.add(l)
+    for l in y_pred:
+        labels.add(l)
+    labels = list(labels)
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    errors_dict = {}
+    errors_dict_single = {}
+    errors = {}
+    rows, cols = np.nonzero(cm)
+    for i in range(len(rows)):
+        if rows[i] == cols[i]:
+            continue
+        v = cm[rows[i]][cols[i]]
+        if v == 1:
+            continue
+        l1 = labels[rows[i]]
+        l2 = labels[cols[i]]
+        if l1 not in errors_dict_single:
+            errors_dict_single[l1] = 0
+        if l1 not in errors:
+            errors[l1] = {}
+        errors[l1][l2] = v
+        errors_dict_single[l1] += v
+        errors_dict[l1 + " " + l2] = v
+        # print(l1, l2, v)
+    sorted_x = sorted(errors_dict.items(), key=operator.itemgetter(1), reverse=True)
+    errors_dict = collections.OrderedDict(sorted_x)
+    sorted_x = sorted(errors_dict_single.items(), key=operator.itemgetter(1), reverse=True)
+    errors_dict_single = collections.OrderedDict(sorted_x)
+    errors_dict_ratio = {}
+    for k in errors_dict_single:
+        errors_dict_ratio[k] = errors_dict_single[k] / distribution[k]
+    sorted_x = sorted(errors_dict_ratio.items(), key=operator.itemgetter(1), reverse=True)
+    errors_dict_ratio = collections.OrderedDict(sorted_x)
+    # print(errors_dict)
+    # print(errors_dict_single)
+    # print(errors_dict_ratio)
+    # print(errors)
+
+    for k in list(errors_dict_single.keys())[:5]:
+        print("### " + str(k))
+        print("Total: " + str(distribution[k]))
+        print("Errors: " + str(errors_dict_single[k]))
+        print("Ratio: " + str(errors_dict_ratio[k]))
+        print("Errors matrix: " + str(errors[k]))
+        print()
